8516be7b6eb55bfb0f43917163014324cc2fd1c3
Luis Eduardo Gutiérrez Saenz Pardo <52941810+luisedgtz@users.noreply.github.com>
2021-07-07 17:18:09 -0500

    Implemented Bottom Tab Navigation using Fragments

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        //Fragments
+        final Fragment fragment1 = new HomeFragment();
+        final Fragment fragment2 = new NewPostFragment();
+        final Fragment fragment3 = new UserFragment();
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment1).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+
+                switch (item.getItemId()) {
+                    case R.id.action_new_post:
+                        fragment = fragment2;
+                        break;
+                    case R.id.action_user:
+                        fragment = fragment3;
+                        break;
+                    case R.id.action_home:
+                    default:
+                        fragment = fragment1;
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/instagram/NewPostFragment.java
@@ -0,0 +1,22 @@
+package com.example.instagram;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewPostFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewPostFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_post, container, false);
+    }
+}
